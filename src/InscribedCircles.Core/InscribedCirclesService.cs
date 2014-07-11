5e62789572e2c0a1cfc18f1506d77380a51de300
tlyshak <redlleaf@gmail.com>
2014-07-11 17:13:04 +0300

    Fitting inscribing started

--- src/InscribedCircles.Core/InscribedCirclesService.cs
+++ src/InscribedCircles.Core/InscribedCirclesService.cs
@@ -10,8 +10,12 @@ namespace InscribedCircles.Core
             var points = new List<Point>();
             double currentWidth = gap;
             double currentY = gap + circleRadius;
-            for (var i = 0;; i++)
+            bool isPairedRow;
+            double secondRowWidth = 0;
+            int rowCount = 0;
+            for (; ; rowCount++)
             {
+                isPairedRow = Convert.ToBoolean(rowCount % 2);
                 while (true)
                 {
                     var currentCenterX = currentWidth + circleRadius;
@@ -19,18 +23,44 @@ namespace InscribedCircles.Core
                     if (currentWidth > rectangleWidth) break;
                     points.Add(new Point(currentCenterX, currentY));
                 }
-                var newCircleCenter = GetNewCircleCenter(rectangleHeight, currentY, circleRadius, gap, Convert.ToBoolean(i % 2));
+                if (rowCount == 1)
+                    secondRowWidth = currentWidth;
+                var newCircleCenter = GetNewCircleCenter(rectangleHeight, currentY, circleRadius, gap, isPairedRow);
                 if (newCircleCenter == null) break;
                 currentWidth = newCircleCenter.CenterX;
                 currentY = newCircleCenter.CenterY;
             }
+            var fullHeight = currentY + circleRadius + gap;
+            var apparentWidth = rectangleWidth + (rectangleHeight - fullHeight)/++rowCount;
+            if (apparentWidth - secondRowWidth > 0.1) // more than 0.1(mm) gap
+            {
+                var fitGap = apparentWidth - secondRowWidth;
+                points = new List<Point>();
+                currentWidth = gap;
+                currentY = gap + circleRadius;
+                for (int i = 0; ; i++)
+                {
+                    isPairedRow = Convert.ToBoolean(i % 2);
+                    while (true)
+                    {
+                        var currentCenterX = currentWidth + circleRadius;
+                        currentWidth = currentCenterX + circleRadius + gap;
+                        if (currentWidth > rectangleWidth) break;
+                        points.Add(new Point(currentCenterX, currentY));
+                    }
+                    var newCircleCenter = GetNewCircleCenter(rectangleHeight, currentY, circleRadius, gap, isPairedRow, isPairedRow ? 0 : fitGap);
+                    if (newCircleCenter == null) break;
+                    currentWidth = newCircleCenter.CenterX;
+                    currentY = newCircleCenter.CenterY;
+                }
+            }
             return points;
         }
 
-        private Point GetNewCircleCenter(double rectangleHeight, double currentHeight, double circleRadius, double gap, bool isPairedRow)
+        private Point GetNewCircleCenter(double rectangleHeight, double currentHeight, double circleRadius, double gap, bool isPairedRow, double fitGap = 0)
         {
-            double centerY = currentHeight + 1.5*(2/Math.Sqrt(3)*circleRadius) + gap;
-            double centerX = isPairedRow ? gap : (1.5*gap + circleRadius);
+            double centerY = currentHeight + 1.5*(2/Math.Sqrt(3)*circleRadius) + gap + fitGap;
+            double centerX = isPairedRow ? gap : (1.5*gap + circleRadius) - fitGap;
             return (centerY + circleRadius + gap) <= rectangleHeight ? new Point(centerX, centerY) : null;
         }
     }
